3d18f3995adef4695e0c633f40c52c45192021f8
Gaurav Pant <gauravpgaurav@gmail.com>
2016-02-08 23:57:14 +0530

    Validation MySQL version

--- SQL_Examples/Sql101.sql
+++ SQL_Examples/Sql101.sql
@@ -6,6 +6,8 @@ CREATE TABLE CUSTOMER(
    PRIMARY KEY ( CUSTOMERID )
    );
    
+ ----------------------
+   
 Select * from gpant.CUSTOMER;
 
 CREATE PROCEDURE gpant.updatePhoneNumber(p_customerId numeric , p_phoneNumber numeric)
@@ -16,3 +18,14 @@ CREATE PROCEDURE gpant.updatePhoneNumber(p_customerId numeric , p_phoneNumber nu
 DROP PROCEDURE gpant.updatePhoneNumber
    
 CALL gpant.updatePhoneNumber(875,12345);
+
+----------------------------------------
+
+CREATE table gpant.VERIFYUSER (
+    userName  VARCHAR(16) NOT NULL,
+    passWord  VARCHAR(16) NOT NULL,
+    roll VARCHAR(20),
+    Primary key (userName)
+);
+
+INSERT INTO gpant.VERIFYUSER values("Gaurav","pant","Intern");

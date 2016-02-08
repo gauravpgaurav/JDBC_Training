8026e024176100238c929f23b18c3e9232fca68d
Gaurav Pant <gauravpgaurav@gmail.com>
2016-02-08 11:21:32 +0530

    Getting update count via SQL

--- SQL_Examples/src/com/training/examples/CallableExample.java
+++ SQL_Examples/src/com/training/examples/CallableExample.java
@@ -1,33 +1,32 @@
 package com.training.examples;
+
 import java.sql.*;
 
 public class CallableExample {
 
-	public static void main(String[] args){
-		
-		try{
-			
+	public static void main(String[] args) {
+
+		try {
+
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			
+
 			String url = "jdbc:oracle:thin:system/db@localhost:1521:XE";
-			
+
 			Connection con = DriverManager.getConnection(url);
-			
-			CallableStatement stmt = con.prepareCall("call updatePhoneNumber (?,?)");
-			
+
+			CallableStatement stmt = con.prepareCall("call updatePhoneNumber (?,?,?)");
+
 			stmt.setInt(1, 102);
 			stmt.setLong(2, 11111L);
-		
-			stmt.execute();
-			int count = stmt.getUpdateCount();
-			System.out.println(" \t But Number of Updated Rows = "+count);
+			stmt.registerOutParameter(3, java.sql.Types.NUMERIC);
+			
+			//int count = stmt.getUpdateCount();
+			System.out.println(stmt.execute()+" \t But Number of Updated Rows = " + stmt.getInt(3));
 			// If true resultSet was returned
 			// If false nothing was returned. Hence false returned
 			System.out.println("Thanks for updating Phone Number");
-			
-		}
-		catch(Exception e)
-		{
+
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
